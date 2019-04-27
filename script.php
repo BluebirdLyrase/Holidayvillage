293ec84b1ab5260c9517a6c556e364ccdf9a4593
tewtew023 <38246059+tewtew023@users.noreply.github.com>
2019-04-27 14:56:43 +0700

    asd

---
+++ script.php
@@ -0,0 +1,13 @@
+<?php
+
+function console_log($data) {
+    $output  = "<script>console.log( 'PHP debugger: ";
+    $output .= json_encode(print_r($data, true));
+    $output .= "' );</script>";
+    echo $output;
+  }
+////////////////////////////////////////////////////////////////////////////////////////
+function alert($msg) {
+    echo "<script type='text/javascript'>alert('$msg');</script>";
+}
+?>
