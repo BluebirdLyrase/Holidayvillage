2acaf042ec5042a3c069e750b8b9e75a428ff70f
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-05-05 20:27:59 +0700

    Update Input.php

--- Search/Input.php
+++ Search/Input.php
@@ -5,8 +5,17 @@
 
 
 <form action="Search/input.php" method="post">
-<input type = "hidden" name = "table" value = "">
-<input type = "hidden" name = "select" value = "">
-<input type = "hidden" name = "where" value = "">
+<input type = "hidden" name = "select" value = "
+
+">
+<input type = "hidden" name = "from" value = "
+
+">
+<input type = "hidden" name = "where" value = "
+
+">
+<input type = "hidden" name = "Table" value = "
+
+">
 <input type="submit" name="button" value="ShowAll" />
 </form>
