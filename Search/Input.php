e30852ecc1b67c7c457d3e041e2f555b06882427
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-05-05 20:45:43 +0700

    ShowAll
    ShowEll

--- Search/Input.php
+++ Search/Input.php
@@ -4,18 +4,7 @@
 
 
 
-<form action="Search/input.php" method="post">
-<input type = "hidden" name = "select" value = "
-
-">
-<input type = "hidden" name = "from" value = "
-
-">
-<input type = "hidden" name = "where" value = "
-
-">
-<input type = "hidden" name = "Table" value = "
-
-">
+<form action="Showall.php" method="post">
+<input type = "hidden" name = "select" value = "">
 <input type="submit" name="button" value="ShowAll" />
 </form>
